e78d61f18c6bb7e9f701eb04694696f8d024987e
David Fenster <dfenster@shutterstock.com>
2012-09-13 15:08:41 -0400

    add logError function

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ module.exports = {
   Validation: require('./lib/validation'),
   Generic: require('./lib/generic')
 };
+
+module.exports.logError = function(err, cb) { 
+  if(!err.isLogged) { 
+    err.isLogged = true; 
+    console.error(err); 
+  } 
+  if(cb) cb(err); 
+};
