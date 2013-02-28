0c7f3ad71163944fd150bd1a74e96b1627578ed9
Ben Kovacevich <bkovacevich@shutterstock.com>
2013-02-27 19:29:50 -0500

    add error handleing middleware for express

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   Validation: require('./lib/validation'),
   Generic: require('./lib/generic'),
   HttpStatus: require('./lib/http-status'),
-  Argument: require('./lib/argument')
+  Argument: require('./lib/argument'),
+  middleware: require('./lib/middleware')
 };
 
 module.exports.logError = function(err, cb) { 
