fdda363deba08384892089fa3b7cb4f44ade6350
David Fenster <dfenster@shutterstock.com>
2013-05-07 13:39:54 -0400

    new log function to replace logError

--- index.js
+++ index.js
@@ -12,11 +12,26 @@ module.exports = {
   }
 };
 
+var logErrorDeprecationWarning = false;
 module.exports.logError = function(err, cb) { 
-  if(!err.isLogged) { 
+  if(!logErrorDeprecationWarning) console.warn("logError is deprecated.  Use log instead.");
+  logErrorDeprecationWarning = true;
+
+  if(err && !err.isLogged) { 
     err.isLogged = true; 
     console.error(err); 
   } 
   if(cb) cb(err); 
 };
 
+module.exports.log = function(err, message) {
+  if(typeof err == 'string') err = new module.exports.Generic(err);
+  if(err) {
+    message = message || "A generic error has occurred.";
+    if(err.isLogged) err = new module.exports.Generic(message);
+    else err = new module.exports.Generic(message, err);
+    console.error(err);
+    err.isLogged = true;
+  }
+  return err;
+}
