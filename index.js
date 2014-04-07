aa8fc73797e426cfe7e6618b02ae6d172fd64211
David Fenster <dfenster@shutterstock.com>
2014-04-07 14:30:09 -0400

    refactor HttpStatusError for connect err handling

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 module.exports = {
-  Validation: require('./lib/validation'),
-  Generic: require('./lib/generic'),
-  HttpStatus: require('./lib/http-status'),
-  NotSupported: require('./lib/not-supported'),
-  Argument: require('./lib/argument'),
-  ArgumentNull: require('./lib/argumentNull'),
-  AlreadyInUse: require('./lib/alreadyInUse'),
-  NotPermitted: require('./lib/notPermitted'),
-  AuthenticationRequired: require('./lib/authenticationRequired'),
-  middleware: {
-    errorHandler: require('./lib/middleware/errorHandler'),
-    crashProtector: require('./lib/middleware/crashProtector')
-  }
+    Validation: require('./lib/validation'),
+    Generic: require('./lib/generic'),
+    HttpStatus: require('./lib/http-status'),
+    NotSupported: require('./lib/not-supported'),
+    Argument: require('./lib/argument'),
+    ArgumentNull: require('./lib/argumentNull'),
+    AlreadyInUse: require('./lib/alreadyInUse'),
+    NotPermitted: require('./lib/notPermitted'),
+    AuthenticationRequired: require('./lib/authenticationRequired'),
+    middleware: {
+      errorHandler: require('./lib/middleware/errorHandler'),
+      crashProtector: require('./lib/middleware/crashProtector')
+    }
 };
 
 var logErrorDeprecationWarning = false;
-module.exports.logError = function(err, cb) { 
-  if(!logErrorDeprecationWarning) console.warn("logError is deprecated.  Use log instead.");
-  logErrorDeprecationWarning = true;
+module.exports.logError = function(err, cb) {
+    if (!logErrorDeprecationWarning) console.warn("logError is deprecated.  Use log instead.");
+    logErrorDeprecationWarning = true;
 
-  if(err && !err.isLogged) { 
-    err.isLogged = true; 
-    console.error(err); 
-  } 
-  if(cb) cb(err); 
+    if (err && !err.isLogged) {
+        err.isLogged = true;
+        console.error(err);
+    }
+    if (cb) cb(err);
 };
 
 module.exports.log = function(err, message) {
-  if(typeof err == 'string') {
-    err = new module.exports.Generic(err);
-  } else {
-    if (message) {
-      err.message = message;
+    if (typeof err == 'string') {
+        err = new module.exports.Generic(err);
+    } else {
+        if (message) {
+            err.message = message;
+        }
+        err = module.exports._prependCurrentStack(err);
     }
-    err = module.exports._prependCurrentStack(err);
-  }
-  if(err) {
-    console.error(err && err.stack || err);
-    err.isLogged = true;
-  }
-  return err;
+    if (err) {
+        console.error(err && err.stack || err);
+        err.isLogged = true;
+    }
+    return err;
 }
 
 module.exports._prependCurrentStack = function(err) {
-  // skip the first three lines, because they're just noise
-  var stackToPrepend = (new Error()).stack.split("\n").slice(3);
-  var mainStack = err.stack.split("\n");
-  var errTitle = mainStack.shift();
-  err.stack = [errTitle].concat(stackToPrepend, "====", mainStack).join("\n");
-  return err;
+    // skip the first three lines, because they're just noise
+    var stackToPrepend = (new Error()).stack.split("\n").slice(3);
+    var mainStack = err.stack.split("\n");
+    var errTitle = mainStack.shift();
+    err.stack = [errTitle].concat(stackToPrepend, "====", mainStack).join("\n");
+    return err;
 };
