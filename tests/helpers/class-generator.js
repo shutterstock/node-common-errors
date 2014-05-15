f16a79e2a0b433168b125d706789d639636b72d5
David Fenster <dfenster@shutterstock.com>
2014-05-15 07:18:56 -0400

    add tests for class-generator.js

---
+++ tests/helpers/class-generator.js
@@ -0,0 +1,63 @@
+var assert = require('assert');
+var classGenerator = require('../../lib/helpers/class-generator');
+
+describe('classGenerator', function(){
+  it("should work", function(){
+    var Err = classGenerator("Err", {args: ['message', 'inner_error', 'arg1', 'arg2']});
+    assert.ok(Err, 'Error returned');
+    assert.equal(Err.length, 4, "has proper constructor arguments");
+    assert.equal(Err.super_, Error, "is instanceof Error");
+    assert.equal(Err.name, "Err", "has proper name");
+  });
+
+  it('should be safe to use a name - strings only', function(){
+    var caught_error;
+    try {
+      var Err = classGenerator(2);
+    } catch(e) {
+      caught_error = e;
+    }
+    assert.ok(caught_error, "using a non-string is unsafe.");
+  });
+
+  it('should be safe to use a name - word characters only', function(){
+    var caught_error;
+    try {
+      var Err = classGenerator("New Error");
+    } catch(e) {
+      caught_error = e;
+    }
+    assert.ok(caught_error, "using a non-string is unsafe.");
+  });
+
+  it('should be safe to use custom arguments - array only', function(){
+    var caught_error;
+    try {
+      var Err = classGenerator("NewError", {args: {}});
+    } catch(e) {
+      caught_error = e;
+    }
+    assert.ok(caught_error, "using a non-array is unsafe.");
+  });
+
+  it('should be safe to use custom arguments - array of strings only', function(){
+    var caught_error;
+    try {
+      var Err = classGenerator("NewError", {args: ['arg1', 2]});
+    } catch(e) {
+      caught_error = e;
+    }
+    assert.ok(caught_error, "using a non-strings in args array is unsafe");
+  });
+
+  it('should be safe to use custom arguments - array of strings only', function(){
+    var caught_error;
+    try {
+      var Err = classGenerator("NewError", {args: ['arg1', 'arg 2']});
+    } catch(e) {
+      caught_error = e;
+    }
+    assert.ok(caught_error, "using non-word characters in args array is unsafe");
+  });
+
+});
