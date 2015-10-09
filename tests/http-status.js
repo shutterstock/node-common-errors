f68bbcaefab5d32f6c584e6630c4b187e6ee454e
David Fenster <dfenster@shutterstock.com>
2015-10-09 09:50:46 -0400

    ability to create error instances without new keyword

---
+++ tests/http-status.js
@@ -0,0 +1,49 @@
+var assert = require('assert');
+var HttpStatusError = require('../').HttpStatusError;
+
+describe("HttpStatusError", function(){
+  function performBasicAssertions(error){
+    assert.equal(error.name, "HttpStatusError"), 'Its name is correct.';
+    console.log(error, error.stack)
+    assert.ok(new RegExp(error.name + ": " + error.message.replace(/\)/g, '\\)').replace(/\(/g, '\\(') + "\n(.*\n)+").test(error.stack), "Stack is good");
+    assert.ok(error instanceof Error, Error, "It is an instanceof Error");
+  }
+
+  it("should work with status code and message", function(){
+    var error = new HttpStatusError(403, "You got a 403");
+    assert.equal(error.message, "You got a 403");
+    assert.equal(error.status, 403);
+    assert.equal(error.status_code, 403);
+    assert.equal(error.statusCode, 403);
+    performBasicAssertions(error);
+  });
+
+  it("should work with status code", function(){
+    var error = new HttpStatusError(403);
+    assert.equal(error.message, "(403) Forbidden!");
+    assert.equal(error.status, 403);
+    assert.equal(error.status_code, 403);
+    assert.equal(error.statusCode, 403);
+    performBasicAssertions(error);
+  });
+
+  it("should work with status code and message without new", function(){
+    var error = HttpStatusError(403, "You got a 403");
+    assert.equal(error.message, "You got a 403");
+    assert.equal(error.status, 403);
+    assert.equal(error.status_code, 403);
+    assert.equal(error.statusCode, 403);
+    performBasicAssertions(error);
+  });
+
+  it("should work with status code without new", function(){
+    var error = HttpStatusError(403);
+    assert.equal(error.message, "(403) Forbidden!");
+    assert.equal(error.status, 403);
+    assert.equal(error.status_code, 403);
+    assert.equal(error.statusCode, 403);
+    performBasicAssertions(error);
+  });
+
+
+});
--- tests/support/index.js
+++ tests/support/index.js
@@ -8,18 +8,32 @@ module.exports.testError = function testError(name, opts){
   opts.extends = opts.extends || Error;
   opts.full_name = opts.full_name || name;
 
+  function performAssertions(Err, error) {
+    assert.equal(error.name, name), 'Its name is correct.';
+    assert.equal(error.message, opts.message_to_assert);
+    assert.ok(new RegExp(error.name + ": " + error.message + "\n(.*\n)+").test(error.stack), "Stack is good");
+    assert.equal(Err.super_.name, opts.extends.name, "It is an instance of" + opts.extends.name);
+    assert.ok(error instanceof Error, opts.extends, "It is an instanceof " + opts.extends.name);  
+  }  
+
   describe(name, function(){
+    var Err = errors;
+    (opts.full_name).split('.').forEach(function(dir){ Err = Err[dir]; });
+
     it("should work", function(){
-      var Err = errors;
-      (opts.full_name).split('.').forEach(function(dir){ Err = Err[dir]; });
       assert.ok(Err, name + " exists.");
       var inner_error = new Error("inner error");
       var error = new Err(opts.message);
-      assert.equal(error.name, name), 'Its name is correct.';
-      assert.equal(error.message, opts.message_to_assert);
-      assert.ok(new RegExp(error.name + ": " + error.message + "\n(.*\n)+").test(error.stack), "Stack is good");
-      assert.equal(Err.super_.name, opts.extends.name, "It is an instance of" + opts.extends.name);
-      assert.ok(error instanceof Error, opts.extends, "It is an instanceof " + opts.extends.name);
+
+      performAssertions(Err, error);
+    });
+
+    it("should work without new", function(){
+      assert.ok(Err, name + " exists.");
+      var inner_error = new Error("inner error");
+      var error = Err(opts.message);
+
+      performAssertions(Err, error);
     });
   });
 }
